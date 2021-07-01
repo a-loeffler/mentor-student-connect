abc8dc71bc5b49f27a560f7b452e3d0afd5ad4c1
Andrew Loeffler <aloeffler@gmail.com>
2021-06-30 23:32:01 -0400

    add text input field for new messages

--- frontend/src/components/MessagesWidget/TextMessages.js
+++ frontend/src/components/MessagesWidget/TextMessages.js
@@ -1,8 +1,11 @@
+import { useState } from 'react';
+
 import TextBubble from './TextBubble';
 
 
 const TextMessages = ({activeTexts, userId}) => {
 
+    const [messageText, setMessageText] = useState("")
 
     //to-do: get individual messages out of the messages object
 
@@ -12,6 +15,10 @@ const TextMessages = ({activeTexts, userId}) => {
     return (
         <div className="text-messages-container">
             {activeTexts.map(text => <TextBubble bubbleType={text.sender_id === userId ? "outgoing" : "incoming"} contents={text.contents}/>)}
+            <form className="text-input-form">
+                <input className="text-input-field" type="text" defaultValue={"Add message"} value={messageText} onChange={e => setMessageText(e.target.value)} autoFocus></input>
+                <button className="text-submit-button">Send</button>
+            </form>
         </div>
     )
 }
