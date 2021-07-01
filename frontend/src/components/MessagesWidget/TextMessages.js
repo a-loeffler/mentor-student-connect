e33bf4be1a5e3304dfc7a7b786dca307365d2391
Andrew Loeffler <aloeffler@gmail.com>
2021-06-30 23:36:40 -0400

    start text submit actions

--- frontend/src/components/MessagesWidget/TextMessages.js
+++ frontend/src/components/MessagesWidget/TextMessages.js
@@ -12,12 +12,18 @@ const TextMessages = ({activeTexts, userId}) => {
     console.log("activeTexts", activeTexts)
     console.log("userId", userId)
 
+    const sendText = (e) => {
+        e.preventDefault();
+
+        //to-do: dispatch thunk to post to database; rerender current position with new text
+    }
+
     return (
         <div className="text-messages-container">
             {activeTexts.map(text => <TextBubble bubbleType={text.sender_id === userId ? "outgoing" : "incoming"} contents={text.contents}/>)}
-            <form className="text-input-form">
+            <form className="text-input-form" onSubmit={e => sendText(e)}>
                 <input className="text-input-field" type="text" defaultValue={"Add message"} value={messageText} onChange={e => setMessageText(e.target.value)} autoFocus></input>
-                <button className="text-submit-button">Send</button>
+                <button className="text-submit-button" onClick={e => sendText(e)}>Send</button>
             </form>
         </div>
     )
