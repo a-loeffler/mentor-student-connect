b655dd5de2431506b2a7264328599b0da4e84522
Andrew Loeffler <aloeffler@gmail.com>
2021-07-10 18:18:38 -0400

    have mostly functioning map interface

--- frontend/src/components/FindMentorsWidget/MentorCard.js
+++ frontend/src/components/FindMentorsWidget/MentorCard.js
@@ -1,13 +1,34 @@
+import './index.css'
+
+
+const MentorCard = ({mentorName, mentorZipcode, mentorId}) => {
 
 
 
-const MentorCard = () => {
-
-
+    const connectionRequestActions = (e) => {
+        e.preventDefault()
+    }
 
     return (
         <div className="mentor-card-container">
-            <h1>Mock MentorCard</h1>
+            <div className="mentor-card-title-container">
+                <h1 className="mentor-card-title">Local Mentor</h1>
+            </div>
+            <div className="mentor-card-content-container">
+                <div className="mentor-card-picture-container">
+                    <img className="mentor-card-picture" src="images/profile.svg" alt="mentor's profile image"></img>
+                </div>
+
+                <div className="mentor-card-info-container">
+                    <div className="mentor-info">
+                        <h1 className="mentor-card-name">{mentorName}</h1>
+                        <h2 className="mentor-card-zipcode">{mentorZipcode}</h2>
+                    </div>
+                    <div className="mentor-card-button-container">
+                        <button className="mentor-card-connection-request-button" onClick={e => connectionRequestActions(e)} >Send Connection Request</button>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
